cffa3dc1a9665373e7ea572bf91db69219f5426f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-09-21 16:31:03 -0700

    Update app/(dashboard)/page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,17 +1,17 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { File, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { ProductsTable } from './products-table';
-import { getProducts } from '@/lib/db';
+import { ObituariesTable } from './obituaries-table';
+import { getObituaries } from '@/lib/db';
 
-export default async function ProductsPage({
+export default async function ObituariesPage({
   searchParams
 }: {
   searchParams: { q: string; offset: string };
 }) {
   const search = searchParams.q ?? '';
   const offset = searchParams.offset ?? 0;
-  const { products, newOffset, totalProducts } = await getProducts(
+  const { obituaries, newOffset, totalObituaries } = await getObituaries(
     search,
     Number(offset)
   );
@@ -21,11 +21,8 @@ export default async function ProductsPage({
       <div className="flex items-center">
         <TabsList>
           <TabsTrigger value="all">All</TabsTrigger>
-          <TabsTrigger value="active">Active</TabsTrigger>
-          <TabsTrigger value="draft">Draft</TabsTrigger>
-          <TabsTrigger value="archived" className="hidden sm:flex">
-            Archived
-          </TabsTrigger>
+          <TabsTrigger value="proofread">Proofread</TabsTrigger>
+          <TabsTrigger value="not-proofread">Not Proofread</TabsTrigger>
         </TabsList>
         <div className="ml-auto flex items-center gap-2">
           <Button size="sm" variant="outline" className="h-8 gap-1">
@@ -37,16 +34,16 @@ export default async function ProductsPage({
           <Button size="sm" className="h-8 gap-1">
             <PlusCircle className="h-3.5 w-3.5" />
             <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-              Add Product
+              Add Obituary
             </span>
           </Button>
         </div>
       </div>
       <TabsContent value="all">
-        <ProductsTable
-          products={products}
+        <ObituariesTable
+          obituaries={obituaries}
           offset={newOffset ?? 0}
-          totalProducts={totalProducts}
+          totalObituaries={totalObituaries}
         />
       </TabsContent>
     </Tabs>
