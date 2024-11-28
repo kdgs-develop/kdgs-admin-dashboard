f01bf43a3bee3c88c567d44e933c6a46e8bfcd1c
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-11-27 21:51:16 -0800

    Update actions.ts

--- app/(dashboard)/obituary/[reference]/actions.ts
+++ app/(dashboard)/obituary/[reference]/actions.ts
@@ -3,6 +3,8 @@
 import minioClient from '@/lib/minio-client';
 import { prisma } from '@/lib/prisma';
 import { Prisma } from '@prisma/client';
+import { createHash } from 'crypto';
+import { notFound } from "next/navigation";
 
 type ObituaryWithRelations = Prisma.ObituaryGetPayload<{
   include: {
@@ -67,4 +69,66 @@ export async function fetchImagesForObituaryAction(
     stream.on('error', reject);
     stream.on('end', () => resolve(images));
   });
+}
+
+export async function generatePublicHashAction(obituaryId: number): Promise<string> {
+  const hash = createHash('sha256')
+    .update(`${obituaryId}-${process.env.HASH_SECRET}`)
+    .digest('hex');
+
+  // Check if the hash already exists
+  const existingHash = await prisma.obituary.findUnique({
+    where: { publicHash: hash }
+  });
+
+  if (existingHash) {
+    return existingHash?.publicHash!;
+  }
+
+  await prisma.obituary.update({
+    where: { id: obituaryId },
+    data: { publicHash: hash }
+  });
+
+  return hash;
+}
+
+export async function getPublicObituaryByHash(hash: string) {
+  console.log('getPublicObituaryByHash', hash);
+  if (!hash) return null;
+
+  try {
+    const obituary = await prisma.obituary.findUnique({
+      where: { 
+        publicHash: hash,
+      },
+      include: {
+        title: true,
+        birthCity: { 
+          include: { 
+            country: true 
+          } 
+        },
+        deathCity: { 
+          include: { 
+            country: true 
+          } 
+        },
+        cemetery: true,
+        periodical: true,
+        fileBox: true,
+        relatives: true,
+        alsoKnownAs: true,
+      },
+    });
+
+    // Log for debugging
+    console.log('Fetching obituary with hash:', hash);
+    console.log('Found obituary:', obituary ? 'yes' : 'no');
+
+    return obituary;
+  } catch (error) {
+    console.error('Error in getPublicObituaryByHash:', error);
+    throw error; // Let the client handle the error
+  }
 }
