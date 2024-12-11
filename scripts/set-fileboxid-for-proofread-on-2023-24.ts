c52c41b09ce117c457b36b0324ec795434fbf3e1
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-12-11 15:36:32 -0800

    Obituaries proofread on 2023 24 updated to fileBoxId 1

--- scripts/set-fileboxid-for-proofread-on-2023-24.ts
+++ scripts/set-fileboxid-for-proofread-on-2023-24.ts
@@ -1,27 +1,27 @@
-const { PrismaClient } = require('@prisma/client');
+// const { PrismaClient } = require('@prisma/client');
 
-const prisma_ = new PrismaClient();
+// const prisma_ = new PrismaClient();
 
-async function setFileBoxForProofread2023And2024Obituaries() {
-  try {
-    const result = await prisma_.obituary.updateMany({
-      where: {
-        proofreadDate: {
-          gte: new Date(Date.UTC(2023, 0, 1)), // January 1, 2023
-          lt: new Date(Date.UTC(2025, 0, 1))   // January 1, 2025
-        },
-      },
-      data: {
-        fileBoxId: 1,
-      },
-    });
+// async function setFileBoxForProofread2023And2024Obituaries() {
+//   try {
+//     const result = await prisma_.obituary.updateMany({
+//       where: {
+//         proofreadDate: {
+//           gte: new Date(Date.UTC(2023, 0, 1)), // January 1, 2023
+//           lt: new Date(Date.UTC(2025, 0, 1))   // January 1, 2025
+//         },
+//       },
+//       data: {
+//         fileBoxId: 1,
+//       },
+//     });
 
-    console.log(`Set fileBoxId to 1 for ${result.count} obituaries proofread in 2023 and 2024.`);
-  } catch (error) {
-    console.error('Error setting obituary fileBoxIds:', error);
-  } finally {
-    await prisma_.$disconnect();
-  }
-}
+//     console.log(`Set fileBoxId to 1 for ${result.count} obituaries proofread in 2023 and 2024.`);
+//   } catch (error) {
+//     console.error('Error setting obituary fileBoxIds:', error);
+//   } finally {
+//     await prisma_.$disconnect();
+//   }
+// }
 
-setFileBoxForProofread2023And2024Obituaries();
+// setFileBoxForProofread2023And2024Obituaries();
