e2173f99026d75df06aac5669ae799725016fb8f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-01-16 02:58:49 -0800

    Update update-batch.ts

--- scripts/update-batch.ts
+++ scripts/update-batch.ts
@@ -1,16 +1,16 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma_ = new PrismaClient();
+// const { PrismaClient } = require('@prisma/client');
+// const prisma_ = new PrismaClient();
 
-async function updateBatchNumber() {      
-  await prisma_.batchNumber.update({
-    where: { 
-      id: 'cm5z31iph0001drfuz6guw4g9' 
-    },
-    data: { 
-      createdById: 'user_2o5EFU6w79krmQMxtdDo5ona5wp' 
-    }
-  });
-  console.log('Batch number updated successfully');
-}
+// async function updateBatchNumber() {      
+//   await prisma_.batchNumber.update({
+//     where: { 
+//       id: 'cm5z31iph0001drfuz6guw4g9' 
+//     },
+//     data: { 
+//       createdById: 'user_2o5EFU6w79krmQMxtdDo5ona5wp' 
+//     }
+//   });
+//   console.log('Batch number updated successfully');
+// }
 
-updateBatchNumber();
+// updateBatchNumber();
