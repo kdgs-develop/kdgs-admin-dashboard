e28fdde05f56ead1db8aa98c232bdb415fbd2cec
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-01-22 17:20:48 -0800

    Update relationship-administration.tsx

--- app/(dashboard)/setup/relationship-administration.tsx
+++ app/(dashboard)/setup/relationship-administration.tsx
@@ -272,7 +272,7 @@ export function RelationshipAdministration() {
                 </div>
               </div>
 
-              <div className="flex justify-center space-x-2">
+              <div className="flex justify-end space-x-2">
                 <Button
                   variant="outline"
                   size="sm"
