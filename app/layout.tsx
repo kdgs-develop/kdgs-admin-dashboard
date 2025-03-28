3a20035e21125b393922cbc631cad0bd2fa11ce2
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-03-27 19:54:58 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,12 @@ export const metadata = {
   title: "KDGS Admin Dashboard",
   description:
     "Secure administrative interface for the Kelowna and District Genealogical Society. Manage obituaries, member records, and genealogical resources efficiently with this Next.js-powered dashboard, featuring Clerk authentication, Postgres database, and a responsive Tailwind CSS design.",
-  copyright:
-    "© 2025 Kelowna & District Genealogical Society | Developed by Javier Gongora o/a Vyoniq Technologies",
-  creator: "Kelowna & District Genealogical Society",
+  copyright: `© ${new Date().getFullYear()} Kelowna & District Genealogical Society | Developed by Javier Gongora — Vyoniq Technologies`,
+  creator: "Kelowna & District Genealogical Society"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
