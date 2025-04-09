122d694f6127c0ae3a98a6b7e84c3388bd76b25c
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-04-09 16:55:04 -0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,16 +4,16 @@ export function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 right-0 py-4 text-center text-sm text-gray-500 bg-white border-t">
       <span className="text-sm text-gray-500">
-        © {currentYear} Kelowna & District Genealogical Society | Developed by
-        Javier Gongora —{" "}
-        {/* <a
-          href="https://vyoniq.com"
+        © {currentYear} Kelowna & District Genealogical Society{" "}
+        <a
+          href="https://kdgs.ca"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-gray-950 hover:underline"
-        > */}
-        Vyoniq Technologies
-        {/* </a> */}
+          className="text-gray-700 hover:underline"
+        >
+          kdgs.ca
+        </a>{" "}
+        | Developed by Javier Gongora — Vyoniq Technologies
       </span>
     </footer>
   );
