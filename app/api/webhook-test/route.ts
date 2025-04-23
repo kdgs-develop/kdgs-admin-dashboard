45ab7af89fa61d8971684db5732f45e283c08afe
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-04-23 14:12:53 -0700

    Delete route.ts

--- app/api/webhook-test/route.ts
+++
@@ -1,9 +0,0 @@
-export async function POST(request: Request) {
-  console.log("Test webhook endpoint hit!");
-  return new Response(JSON.stringify({ received: true }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
-}
