58d592660a4e51056388efa20ace774414534c3f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-04-23 17:54:16 -0700

    Update page.tsx

--- app/(dashboard)/setup/page.tsx
+++ app/(dashboard)/setup/page.tsx
@@ -4,33 +4,35 @@ import {
   CardDescription,
   CardHeader,
   CardTitle
-} from '@/components/ui/card';
-import { prisma } from '@/lib/prisma';
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-import { AdminBackup } from './admin-backup';
-import { BatchNumberAdministration } from './batch-number-administration';
-import { BulkUpload } from './bulk-upload';
-import { CemeteryAdministration } from './cemetery-administration';
-import { CountryAdministration } from './country-administration';
-import { FileBoxAdministration } from './filebox-administration';
-import { GenealogistAdministration } from './genealogist-administration';
-import { LocationAdministration } from './location-administration';
-import { PeriodicalAdministration } from './periodical-administration';
-import { RelationshipAdministration } from './relationship-administration';
-import { TitleAdministration } from './title-administration';
+} from "@/components/ui/card";
+import { prisma } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { AdminBackup } from "./admin-backup";
+import { BatchNumberAdministration } from "./batch-number-administration";
+import { BulkUpload } from "./bulk-upload";
+import { CemeteryAdministration } from "./cemetery-administration";
+import { CountryAdministration } from "./country-administration";
+import { FileBoxAdministration } from "./filebox-administration";
+import { GenealogistAdministration } from "./genealogist-administration";
+import { LocationAdministration } from "./location-administration";
+import { OrdersAdministration } from "./orders-administration";
+import { PeriodicalAdministration } from "./periodical-administration";
+import { RelationshipAdministration } from "./relationship-administration";
+import { TitleAdministration } from "./title-administration";
 
 export default async function SetupPage() {
   const { userId } = auth();
-  if (!userId) redirect('/sign-in');
+  if (!userId) redirect("/sign-in");
 
   const user = await prisma.genealogist.findUnique({
     where: { clerkId: userId }
   });
 
-  if (user?.role !== 'ADMIN' && user?.role !== 'PROCESS_MANAGER') redirect('/dashboard');
+  if (user?.role !== "ADMIN" && user?.role !== "PROCESS_MANAGER")
+    redirect("/dashboard");
 
-  const isAdmin = user?.role === 'ADMIN';
+  const isAdmin = user?.role === "ADMIN";
 
   return (
     <div className="container mx-auto px-4 max-w-[calc(4xl)]">
@@ -47,6 +49,7 @@ export default async function SetupPage() {
               {isAdmin && (
                 <>
                   <BulkUpload />
+                  <OrdersAdministration />
                   <CountryAdministration />
                   <LocationAdministration />
                   <PeriodicalAdministration />
@@ -57,7 +60,6 @@ export default async function SetupPage() {
                   <AdminBackup />
                 </>
               )}
-
             </CardContent>
           </CardHeader>
         </Card>
