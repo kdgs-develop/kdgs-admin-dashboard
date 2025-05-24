92a19d66578a0144ec5b8df683a3d04c3d6b05df
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-05-24 04:03:30 -0700

    Update new-obituary-dialog-trigger.tsx

--- app/public/search/components/new-obituary-dialog-trigger.tsx
+++ app/public/search/components/new-obituary-dialog-trigger.tsx
@@ -24,11 +24,11 @@ export function NewObituaryDialogTrigger() {
           Submit
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] flex flex-col max-h-[85vh]">
+      <DialogContent className="sm:max-w-[425px] flex flex-col max-h-[85vh] p-6">
         <DialogHeader>
           <DialogTitle>Submit</DialogTitle>
         </DialogHeader>
-        <div className="overflow-y-auto flex-grow pt-4">
+        <div className="overflow-y-auto flex-grow px-2 pt-2">
           <NewObituaryForm />
         </div>
       </DialogContent>
