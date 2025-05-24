f0fe52a8f0ee436ac846b4a03e22818b69997f06
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-05-24 03:49:57 -0700

    Create new-obituary-dialog-trigger.tsx

---
+++ app/public/search/components/new-obituary-dialog-trigger.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/components/ui/dialog";
+import { NewObituaryForm } from "./contact-forms/new-obituary-form";
+import { Button } from "@/components/ui/button";
+
+export function NewObituaryDialogTrigger() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="link"
+          className="p-0 h-auto font-medium text-[#003B5C] hover:text-green-600 underline"
+        >
+          Submit
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px] flex flex-col max-h-[85vh]">
+        <DialogHeader>
+          <DialogTitle>Submit</DialogTitle>
+        </DialogHeader>
+        <div className="overflow-y-auto flex-grow pt-4">
+          <NewObituaryForm />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
