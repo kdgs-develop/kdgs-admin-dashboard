ea08cd27f8af4e059980b7c62a4952b5c3e10162
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-09-21 00:02:40 -0700

    Create page.tsx

---
+++ app/dashboard/reports/page.tsx
@@ -0,0 +1,9 @@
+import { ReportsTable } from './reports-table';
+
+export default function ReportsPage() {
+  return (
+    <div className="container mx-auto p-4 max-w-[calc(4xl)]">
+      <ReportsTable />
+    </div>
+  );
+}
