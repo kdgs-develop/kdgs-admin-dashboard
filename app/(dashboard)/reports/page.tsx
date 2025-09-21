13fadccaf41f3a2c2e1fe5c29535a89b485900d5
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-09-21 00:01:06 -0700

    Delete page.tsx

--- app/(dashboard)/reports/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ReportsTable } from './reports-table';
-
-export default function ReportsPage() {
-  return (
-    <div className="container mx-auto p-4 max-w-[calc(4xl)]">
-      <ReportsTable />
-    </div>
-  );
-}
